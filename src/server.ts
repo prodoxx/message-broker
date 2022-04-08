54645c6551ded9cdd9def2bf8c56a7081cb472c4
prodoxx <reggie.escobar94@gmail.com>
2022-04-08 17:35:54 -0500

    Updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
+// load .env file
+require('dotenv').config();
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
-
+import { messageBrokerQueue } from './queues';
+import { messageBrokerWorker } from './workers';
+import { Server } from 'http';
 import { gracefulShutdown } from './helpers/graceful-shutdown';
 
 const expressServer = express();
@@ -16,12 +21,18 @@ expressServer.use(helmet());
 // set entry route for api
 expressServer.use('/v1', require('./routes'));
 
-// start expressServer
-const httpServer = expressServer.listen(expressServer.get('port'), () => {
-    console.log('expressServer is running at port %d in %s mode', expressServer.get('port'), expressServer.get('env'));
-    console.log('Press CTRL-C to stop\n');
+let httpServer: Server;
+const PORT = process.env.PORT || 3000;
+
+// start the queue before starting the server.
+messageBrokerQueue.resume().then(() => {
+    // start live server
+    httpServer = expressServer.listen(PORT, () => {
+        console.log('MessageBroker API is running at port %d in %s mode', PORT, expressServer.get('env'));
+        console.log('Press CTRL-C to stop\n');
+    });
 });
 
-// handle shutdown gracefully
-process.on('SIGTERM', () => gracefulShutdown(httpServer, null, null));
-process.on('SIGINT', () => gracefulShutdown(httpServer, null, null));
+// handle services shutdown gracefully
+process.on('SIGTERM', () => gracefulShutdown(httpServer, messageBrokerQueue, messageBrokerWorker));
+process.on('SIGINT', () => gracefulShutdown(httpServer, messageBrokerQueue, messageBrokerWorker));
