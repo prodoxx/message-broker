c7e8d48bfe236f4e8743af4550438d44d507bce3
prodoxx <reggie.escobar94@gmail.com>
2022-04-12 08:17:24 +0800

    Removed rate limters from api routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,25 +1,13 @@
 import express from 'express';
 import { messageController } from '../controllers';
-import { rateLimit } from 'express-rate-limit';
 
 const router = express.Router();
 
-// rate limiting config
-const generalRateLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
-});
-
-const healthCheckRateLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 1 hour window
-    max: 10000 // limit each IP to 100 requests per windowMs
-});
-
 // routes
-router.get('/health-check', healthCheckRateLimiter, (_req, res) => res.status(200).json({ message: 'OK' }));
+router.get('/health-check', (_req, res) => res.status(200).json({ message: 'OK' }));
 
 // message api routes
-router.post('/message', generalRateLimiter, messageController.sendMessage);
-router.get('/message', generalRateLimiter, messageController.readMessage);
+router.post('/message', messageController.sendMessage);
+router.get('/message', messageController.readMessage);
 
 module.exports = router;
