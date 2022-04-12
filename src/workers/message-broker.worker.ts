ca5a2300324b198616c54d605b846dbb7d97f717
prodoxx <reggie.escobar94@gmail.com>
2022-04-12 08:18:01 +0800

    Removed worker event listerns

--- src/workers/message-broker.worker.ts
+++ src/workers/message-broker.worker.ts
@@ -1,4 +1,4 @@
-import { Worker, Job } from 'bullmq';
+import { Worker } from 'bullmq';
 import { queueName } from '../queues/message-broker.queue';
 
 // no specified job handler because API request will only get the next job and handle it.
@@ -8,15 +8,6 @@ const messageBrokerWorker = new Worker(queueName, undefined, {
         host: process.env.REDIS_URL,
         port: parseInt(process.env.REDIS_PORT || '10')
     }
-});
-
-// set event handlers
-messageBrokerWorker.on('progress', (job: Job) => {
-    console.log(`Message Broker Worker is processing job id: ${job.id}`);
-});
-
-messageBrokerWorker.on('completed', (job: Job) => {
-    console.log(`Message Broker Worker finished processing job id: ${job.id}`);
 });
 
 export default messageBrokerWorker;
