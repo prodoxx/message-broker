d1b64bca6fc72e846b048d29b3ad49182e515efd
prodoxx <reggie.escobar94@gmail.com>
2022-04-12 08:17:05 +0800

    Updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 // load .env file
-require('dotenv').config();
-
-import express from 'express';
+import 'dotenv/config';
+import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import { messageBrokerQueue } from './queues';
@@ -9,26 +8,28 @@ import { messageBrokerWorker } from './workers';
 import { Server } from 'http';
 import { gracefulShutdown } from './helpers/graceful-shutdown';
 
-const expressServer = express();
+require('dotenv').config();
+
+export const app: Application = express();
 
 // middleware
-expressServer.use(bodyParser.json({ limit: '2mb' }));
-expressServer.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // security
-expressServer.use(helmet());
+app.use(helmet());
 
 // set entry route for api
-expressServer.use('/v1', require('./routes'));
+app.use('/v1', require('./routes'));
 
-let httpServer: Server;
+export let httpServer: Server;
 const PORT = process.env.PORT || 3000;
 
 // start the queue before starting the server.
 messageBrokerQueue.resume().then(() => {
     // start live server
-    httpServer = expressServer.listen(PORT, () => {
-        console.log('MessageBroker API is running at port %d in %s mode', PORT, expressServer.get('env'));
+    httpServer = app.listen(PORT, () => {
+        console.log('MessageBroker API is running at port %d in %s mode', PORT, app.get('env'));
         console.log('Press CTRL-C to stop\n');
     });
 });
